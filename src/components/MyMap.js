400c0cc769fa4ca6de12596b3e14c787e1b89aa4
Waldemar2023 <waldemar.fredrich@gmail.com>
2023-06-30 14:21:06 +0200

    filter function for authors and museums added

--- src/components/MyMap.js
+++ src/components/MyMap.js
@@ -1,21 +1,29 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
 
 const MyMap = ({ lat, lng }) => {
   const position = [lat, lng];
 
   return (
-    <MapContainer
-      center={position}
-      zoom={10}
-      style={{ height: '50vh', width: '100%' }}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      />
-      <Marker position={position}>
-        <Popup>Visit it!</Popup>
-      </Marker>
-    </MapContainer>
+    <div className="map" id="map">
+      <MapContainer center={position} zoom={10}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <Marker position={position}>
+          <Popup>Visit it!</Popup>
+        </Marker>
+      </MapContainer>
+    </div>
   );
 };
 
--- src/pages/Museums.js
+++ src/pages/Museums.js
@@ -1,12 +1,39 @@
+import MyMap from '../components/MyMap';
 import Header from '../components/Header';
+import { Link } from 'react-router-dom';
 
-export default function Museums() {
+export default function Museums({entries}) {
   return (
-    <>
+    <div className="home">
       <Header />
       <main>
-        <h1>Museumspage</h1>
+        <h1>Main</h1>
+        {entries.length > 0 ? (
+          entries
+            .filter((entry) => entry.entryType === 'Museum')
+            .map((entry) => {
+              return (
+                <article className="entry-article" key={entry._id}>
+                  <h1>{entry.title}</h1>
+                  <h2>{entry.description}</h2>
+                  <Link to="/articleDetail">
+                    <img
+                      className="entry-image"
+                      src={entry.image}
+                      alt={entry.title}
+                    />
+                  </Link>
+                  <MyMap lat={52.531377} lng={13.381777} />
+                </article>
+              );
+            })
+        ) : (
+          <h1>Loading...</h1>
+        )}
       </main>
-    </>
+      <footer>
+        <h1>Footer</h1>
+      </footer>
+    </div>
   );
 }
